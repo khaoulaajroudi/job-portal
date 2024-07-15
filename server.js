e3ae3b227c11d012734d305a14745c9d9c09a91d
khaoula ajroudi <94904849+khaoulaajroudi@users.noreply.github.com>
2024-07-15 09:50:03 +0100

    first

---
+++ server.js
@@ -0,0 +1,24 @@
+// importation
+const express=require('express')
+const connectdb=require('./config/connectdb')
+require('dotenv').config()
+const cors = require('cors');
+
+
+// initialisation server
+const app=express()
+
+// connection base Données
+connectdb()
+
+// middelwares
+app.use(cors());
+app.use(express.json())
+app.use('/job',require('./routes/job'))
+app.use('/user',require('./routes/user'))
+
+
+
+const port=process.env.PORT
+// creation du serveur
+app.listen(port,(err)=>err?console.log(err):console.log(`server is running in ${port}`))
